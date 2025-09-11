8469292a40d8616ac5c2fb7b40b3c01653da6fd8
lorette-m <lory_works@outlook.com>
2025-09-11 22:51:40 +0300

    Client-Server Addon
    via TCP (NO PROD!). Edu only

--- gRPC_Learn/client/main.go
+++ gRPC_Learn/client/main.go
@@ -1,1 +1,32 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	echov1 "github.com/lorette-m/GoLearn/gRPC_Learn/gen/go/echo/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials())) // для продакшена используйте TLS
+	if err != nil {
+		log.Fatalf("Dial error: %v", err)
+	}
+	defer conn.Close()
+
+	client := echov1.NewEchoServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	resp, err := client.Echo(ctx, &echov1.EchoRequest{Message: "Привет, gRPC!"})
+	if err != nil {
+		log.Fatalf("Echo error: %v", err)
+	}
+
+	log.Printf("Ответ сервера: %s", resp.GetMessage())
+}
--- gRPC_Learn/server/main.go
+++ gRPC_Learn/server/main.go
@@ -1,14 +1,34 @@
-package server
+package main
 
 import (
 	"context"
+	"log"
+	"net"
 
-	echov1 "github.com/lorette-m/gRPC_Learn/gen/go/echo/v1"
-	_ "google.golang.org/grpc"
+	echov1 "github.com/lorette-m/GoLearn/gRPC_Learn/gen/go/echo/v1"
+	"google.golang.org/grpc"
 )
 
 type server struct {
 	echov1.UnimplementedEchoServiceServer
 }
 
-func (s *server) Echo(ctx context.Context, req *echov1.EchoRequest)
+func (s *server) Echo(ctx context.Context, req *echov1.EchoRequest) (*echov1.EchoResponse, error) {
+	log.Printf("Получен запрос: %s", req.GetMessage())
+	return &echov1.EchoResponse{Message: "Эхо: " + req.GetMessage()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Listen error: %v", err)
+	}
+
+	s := grpc.NewServer()
+	echov1.RegisterEchoServiceServer(s, &server{})
+
+	log.Println("gRPC сервер на :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Serve error: %v", err)
+	}
+}
